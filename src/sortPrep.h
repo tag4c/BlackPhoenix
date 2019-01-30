62a3dc734cc46a61ed9d2c9b223cb1613ebee925
Mr Dallas Tyler Leitner <dtl2d@orion08.cs.mtsu.edu>
2019-01-30 10:30:59 -0600

    updating sort to use vectors

--- src/sortPrep.h
+++ src/sortPrep.h
@@ -8,7 +8,7 @@ long int id;
 double coordinates[3];//std::vector<double> coordinates;
 };
 #endif
-void merge(dataStruct*data, dataStruct *tmp_array, int column, int left, int middle, int right ) ;
+void merge(vector<dataStruct> &data, dataStruct *tmp_array, int column, int left, int middle, int right ) ;
 
-void sortPrep(dataStruct *data, int size, int column);
+void sortPrep(vector<dataStruct> &data, int size, int column);
 void sort(dataStruct *data, dataStruct *temp, int column,int left, int right);
