072c61d47325396f93f71422a6b800636efcd85e
Thomas Goff <tag4c@mtmail.mtsu.edu>
2019-01-30 19:38:42 -0600

    Integrated globalPosition.cpp

--- src/globalPosition.cpp
+++ src/globalPosition.cpp
@@ -1,9 +1,11 @@
 //the function recieves the local percentle position value and calculates for global value
 
-void globalPositionValue (vector< vector<double> >& localPositionValueArray, int& numNodes, vector<double>& globalPositionValue)
+#include "globalPosition.h"
+
+void globalPositionValue (std::vector< std::vector <double> >& localPositionValueArray, int& numNodes, std::vector <double>& globalPositionValue)
 {
   int i,j;
-  Double sumOfPos = Double(0.0);
+  double sumOfPos = double(0.0);
   for(j=0; j<numNodes; j++)
     {
       sumOfPos+=localPositionValueArray[j][0];
@@ -13,7 +15,7 @@ void globalPositionValue (vector< vector<double> >& localPositionValueArray, int
   for (j=1; j<(numNodes-1); j++)
     {
       sumOfPos=0;
-      count=0;
+      int count=0;
       for (i=0;i<numNodes;i++)
 	{
 	  if (localPositionValueArray[i][j]>globalPositionValue[j-1])
