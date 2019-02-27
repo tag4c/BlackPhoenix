ef5d248b8c72f74057af4331426e73f5650018f3
LeitnerD <dtl2d@babbage.mtsu.edu>
2019-02-27 10:12:49 -0600

    updating bins and kd tree

---
+++ src/kdTree.h
@@ -0,0 +1,24 @@
+#include <vector>
+#include "sortPrep.h"
+#include "math.h"
+using namespace std;
+#ifndef DATA_H 
+#define DATA_H
+struct dataStruct
+{
+long long int id;
+double coordinates[3];//std::vector<double> coordinates;
+};
+#endif
+#ifndef DATA_N 
+#define DATA_N
+struct node{
+int id;
+int below;
+double cent[3];
+double min[3];
+double max[3];
+double length; 
+};
+#endif
+void kdTree(vector<dataStruct> &data,int id,int left, int right,node *nodes);
