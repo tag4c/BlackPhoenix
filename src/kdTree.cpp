d4ba637524123bf8a2cb97648f256abcf3610c46
EatingMTSU <yw3c@mtmail.mtsu.edu>
2019-03-13 10:33:36 -0500

    verify the kdtree search

--- src/kdTree.cpp
+++ src/kdTree.cpp
@@ -9,12 +9,20 @@ void kdTree(vector<dataStruct> &data, int id, int left, int right, node *nodes){
   int i,j,k;
   int dim;
   double temp[3];
-  nodes[id].id = id;
-  cout << "id  = " << id << endl;
   nodes[id].below=(right-left+1);
-  cout << "below = " << nodes[id].below << endl;
   //check if only one data point in node
   if(nodes[id].below==1){
+    nodes[id].id = id;
+    nodes[id].cent[0] = data[left].coordinates[0];
+    nodes[id].cent[1] = data[left].coordinates[1];
+    nodes[id].cent[2] = data[left].coordinates[2];
+    nodes[id].min[0] = data[left].coordinates[0];
+    nodes[id].min[1] = data[left].coordinates[1];
+    nodes[id].min[2] = data[left].coordinates[2];
+    nodes[id].max[0] = data[left].coordinates[0];
+    nodes[id].max[1] = data[left].coordinates[1];
+    nodes[id].max[2] = data[left].coordinates[2];
+    nodes[id].length = 0;
     return;}
   nodes[id].min[0]=data[left].coordinates[0];
   nodes[id].min[1]=data[left].coordinates[1];
@@ -22,7 +30,6 @@ void kdTree(vector<dataStruct> &data, int id, int left, int right, node *nodes){
   nodes[id].max[0]=data[left].coordinates[0];
   nodes[id].max[1]=data[left].coordinates[1];
   nodes[id].max[2]=data[left].coordinates[2];
-  cout << "start of find min and max" << endl;
   //find min max
   for(i=left;i<=right;i++){
     if(nodes[id].min[0]>data[i].coordinates[0]){
@@ -44,22 +51,18 @@ void kdTree(vector<dataStruct> &data, int id, int left, int right, node *nodes){
       nodes[id].max[2]=data[i].coordinates[2];
     }
   }
-  cout << "end of find min and max" << endl;
   //find length of x y z
   temp[0]=nodes[id].max[0]-nodes[id].min[0];
   temp[1]=nodes[id].max[1]-nodes[id].min[1];
   temp[2]=nodes[id].max[2]-nodes[id].min[2];
   dim=0;
-  cout << "start of find the leng" << endl;
   //find max length of x y or z
   if(temp[2]>temp[0]&&temp[2]>=temp[1])
     dim=2;
   if(temp[1]>temp[0]&&temp[1]>=temp[2])
     dim=1;
-  cout << "end of find len" << endl;
   //sort along that axis
   sortPrep(data,dim,left,right);
-  cout << "end of sort" << endl;
   //split array in half
   int mid=(right+left)/2;
   //find center points of x y z
@@ -68,14 +71,14 @@ void kdTree(vector<dataStruct> &data, int id, int left, int right, node *nodes){
   nodes[id].cent[2]=(nodes[id].min[2]+nodes[id].max[2])/2;
   double tempC[8];
   //make corners
-  tempC[0]=sqrt(pow(nodes[i].min[0]-nodes[i].cent[0],2)+pow(nodes[i].min[1]-nodes[i].cent[1],2)+pow(nodes[i].min[2]-nodes[i].cent[2],2));
-  tempC[1]=sqrt(pow(nodes[i].min[0]-nodes[i].cent[0],2)+pow(nodes[i].min[1]-nodes[i].cent[1],2)+pow(nodes[i].max[2]-nodes[i].cent[2],2));
-  tempC[2]=sqrt(pow(nodes[i].min[0]-nodes[i].cent[0],2)+pow(nodes[i].max[1]-nodes[i].cent[1],2)+pow(nodes[i].min[2]-nodes[i].cent[2],2));
-  tempC[3]=sqrt(pow(nodes[i].min[0]-nodes[i].cent[0],2)+pow(nodes[i].max[1]-nodes[i].cent[1],2)+pow(nodes[i].max[2]-nodes[i].cent[2],2));
-  tempC[4]=sqrt(pow(nodes[i].max[0]-nodes[i].cent[0],2)+pow(nodes[i].min[1]-nodes[i].cent[1],2)+pow(nodes[i].min[2]-nodes[i].cent[2],2));
-  tempC[5]=sqrt(pow(nodes[i].max[0]-nodes[i].cent[0],2)+pow(nodes[i].min[1]-nodes[i].cent[1],2)+pow(nodes[i].max[2]-nodes[i].cent[2],2));
-  tempC[6]=sqrt(pow(nodes[i].max[0]-nodes[i].cent[0],2)+pow(nodes[i].max[1]-nodes[i].cent[1],2)+pow(nodes[i].min[2]-nodes[i].cent[2],2));
-  tempC[7]=sqrt(pow(nodes[i].max[0]-nodes[i].cent[0],2)+pow(nodes[i].max[1]-nodes[i].cent[1],2)+pow(nodes[i].max[2]-nodes[i].cent[2],2));
+  tempC[0]=sqrt(pow(nodes[id].min[0]-nodes[id].cent[0],2)+pow(nodes[id].min[1]-nodes[id].cent[1],2)+pow(nodes[id].min[2]-nodes[id].cent[2],2));
+  tempC[1]=sqrt(pow(nodes[id].min[0]-nodes[id].cent[0],2)+pow(nodes[id].min[1]-nodes[id].cent[1],2)+pow(nodes[id].max[2]-nodes[id].cent[2],2));
+  tempC[2]=sqrt(pow(nodes[id].min[0]-nodes[id].cent[0],2)+pow(nodes[id].max[1]-nodes[id].cent[1],2)+pow(nodes[id].min[2]-nodes[id].cent[2],2));
+  tempC[3]=sqrt(pow(nodes[id].min[0]-nodes[id].cent[0],2)+pow(nodes[id].max[1]-nodes[id].cent[1],2)+pow(nodes[id].max[2]-nodes[id].cent[2],2));
+  tempC[4]=sqrt(pow(nodes[id].max[0]-nodes[id].cent[0],2)+pow(nodes[id].min[1]-nodes[id].cent[1],2)+pow(nodes[id].min[2]-nodes[id].cent[2],2));
+  tempC[5]=sqrt(pow(nodes[id].max[0]-nodes[id].cent[0],2)+pow(nodes[id].min[1]-nodes[id].cent[1],2)+pow(nodes[id].max[2]-nodes[id].cent[2],2));
+  tempC[6]=sqrt(pow(nodes[id].max[0]-nodes[id].cent[0],2)+pow(nodes[id].max[1]-nodes[id].cent[1],2)+pow(nodes[id].min[2]-nodes[id].cent[2],2));
+  tempC[7]=sqrt(pow(nodes[id].max[0]-nodes[id].cent[0],2)+pow(nodes[id].max[1]-nodes[id].cent[1],2)+pow(nodes[id].max[2]-nodes[id].cent[2],2));
   nodes[id].length=tempC[0];
   
   //find distance to farthest corner
@@ -83,6 +86,7 @@ void kdTree(vector<dataStruct> &data, int id, int left, int right, node *nodes){
     if(tempC[i]>nodes[id].length)
       nodes[id].length=tempC[i];
   }
+
   kdTree(data, id*2, left, mid, nodes);
   kdTree(data, id*2+1,mid+1, right, nodes);
 }
@@ -92,21 +96,27 @@ void kdTree_search(node* nodes, double radius, double* sp, int maxNode, vector<i
   double spToCent = dis(sp,nodes[nodeNum].cent);
   double maxNodeLen = nodes[nodeNum].length;
   if(spToCent < radius + maxNodeLen){
-    int subNodeNum1 = nodeNum*2;
-    if(subNodeNum1 > maxNode){
-      exit(1);
+    if(nodes[nodeNum].below == 1){
+      neighPoints.push_back(nodeNum);
     }
-    kdTree_search(nodes,radius,sp,maxNode,neighPoints,subNodeNum1);
-    int subNodeNum2 = nodeNum*2+1;
-    if(subNodeNum2 > maxNode){
-      exit(1);
+    else{
+      int subNodeNum1 = nodeNum*2;
+      if(subNodeNum1 > maxNode){
+	return;
+      }
+      kdTree_search(nodes,radius,sp,maxNode,neighPoints,subNodeNum1);
+      int subNodeNum2 = nodeNum*2+1;
+      if(subNodeNum2 > maxNode){
+	return;
+      }
+      kdTree_search(nodes,radius,sp,maxNode,neighPoints,subNodeNum2);
     }
-    kdTree_search(nodes,radius,sp,maxNode,neighPoints,subNodeNum2);
   }
   else{
-    neighPoints.push_back(nodes[nodeNum].id);
+    return;
   }
-}  
+}
+
 
 double dis(double* p1, double* p2)
 { 
@@ -116,4 +126,19 @@ double dis(double* p1, double* p2)
   }
   distance = sqrt(distance);
   return distance;
+}
+
+void verifySearch(vector<dataStruct>& data, double radius, double* sp){
+  int count = 0;
+  for(int i=0;i<data.size();i++){
+    double p1[3];
+    for(int j=0;j<3;j++){
+      p1[j] = data[i].coordinates[j];
+    }
+    double distance = dis(p1,sp);
+    if(distance < radius){
+      count ++;
+    }
+  }
+  cout << "the total points around = " << count << endl;
 }
--- src/kdTree.h
+++ src/kdTree.h
@@ -6,21 +6,22 @@ using namespace std;
 #define DATA_H
 struct dataStruct
 {
-  long long int id;
-  double coordinates[3];//std::vector<double> coordinates;
+long long int id;
+double coordinates[3];//std::vector<double> coordinates;
 };
 #endif
 #ifndef DATA_N 
 #define DATA_N
 struct node{
-  int id;
-  int below;
-  double cent[3];
-  double min[3];
-  double max[3];
-  double length; 
+int id;
+int below;
+double cent[3];
+double min[3];
+double max[3];
+double length; 
 };
 #endif
 void kdTree(vector<dataStruct>& data,int id,int left, int right,node *nodes);
 void kdTree_search(node* nodes, double radius, double* sp, int MaxNode, vector<int>& neighPoints, int nodeNum=1);
 double dis(double* p1, double* p2);
+void verifySearch(vector<dataStruct>& data, double radius, double* sp);
